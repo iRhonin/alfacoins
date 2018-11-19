c20d9c69ecaa98bb8e493df1c8ef2c986744596b
ArashFatahzade <fatahzade@gmail.com>
2018-11-19 18:21:17 +0330

    Implemented test_bitsend

---
+++ tests/test_bitsend.py
@@ -0,0 +1,30 @@
+from nanohttp import RegexRouteController, json
+
+from .helper import alfacoins_mockup_gateway
+
+
+class Root(RegexRouteController):
+    def __init__(self):
+        super().__init__([
+            ('/api/bitsend', self.bitsend),
+        ])
+
+    @json(verbs=['post'])
+    def bitsend(self):
+        return BITSEND_RESPONSE
+
+
+def test_bitsend():
+    with alfacoins_mockup_gateway(Root()) as gateway:
+        bitsend_id = gateway.bitsend(
+            type='bitcoin',
+            amount=10,
+            options={'address': '3P3QsMVK89JBNqZQv5zMAKG8FK3kJM4rjt'},
+            recipient_name='test_client',
+            recipient_email='abc@abc.com',
+            reference=1,
+        )
+        assert bitsend_id == 1
+
+
+BITSEND_RESPONSE = {'id': '1'}
