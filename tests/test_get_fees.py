4593e10a3acd06dda6e547cb041e5d9618ed7bcf
ArashFatahzade <fatahzade@gmail.com>
2018-11-19 18:11:54 +0330

    Implemented test_fees

---
+++ tests/test_get_fees.py
@@ -0,0 +1,25 @@
+from nanohttp import RegexRouteController, json
+
+from .helper import alfacoins_mockup_gateway
+
+
+class Root(RegexRouteController):
+    def __init__(self):
+        super().__init__([
+            ('/api/fees', self.get_fees),
+        ])
+
+    @json(verbs=['get'])
+    def get_fees(self):
+        return GET_FEES_RESPONSE
+
+
+def test_get_fees():
+    with alfacoins_mockup_gateway(Root()) as gateway:
+        fees = gateway.get_fees()
+        assert fees == GET_FEES_RESPONSE
+
+
+GET_FEES_RESPONSE = {
+    'bitcoin': {'deposit':{'commission':'0.99%','network_fee':'0 BTC'}}
+}
