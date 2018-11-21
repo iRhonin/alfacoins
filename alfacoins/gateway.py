672bc34342c05f8376b5de46a7e31c59c9648303
ArashFatahzade <fatahzade@gmail.com>
2018-11-22 01:43:24 +0330

    Added docstring

--- alfacoins/gateway.py
+++ alfacoins/gateway.py
@@ -8,9 +8,22 @@ from .exceptions import APIException, ServerException
 
 
 class ALFACoins:
+    """
+    Gateway class
 
+    """
     def __init__(self, name=None, secret_key=None, password=None,
                  base_url='https://www.alfacoins.com/api/'):
+        """
+        :param name: Shop Name of API which you assigned when you create the
+            API
+        :param password: Password that you assigned when you create the API,
+            the upper case MD5 hash of it will be stored
+        :param secret_key: Secret key which is given after API was created
+        :param base_url: Base url of APIs, Change it when you want to use a
+            custom server like in tests
+        """
+
         self.base_url = base_url
         self._is_authenticated = False
 
@@ -30,7 +43,7 @@ class ALFACoins:
             .hexdigest() \
             .upper()
 
-    def _request(self, method, uri, params=None, json_data=None):
+    def __request(self, method, uri, params=None, json_data=None):
         url = urljoin(self.base_url, uri)
         response =  request(
             method=method,
@@ -40,7 +53,7 @@ class ALFACoins:
         )
         return response.content.decode('utf-8') ,response.status_code
 
-    def request(self, method, uri, params=dict(), json_data=dict()):
+    def _request(self, method, uri, params=dict(), json_data=dict()):
         if self._is_authenticated:
             json_data.update(
                 name=self.name,
@@ -48,7 +61,7 @@ class ALFACoins:
                 password=self.password
             )
 
-        result, status_code = self._request(
+        result, status_code = self.__request(
             method=method,
             uri=uri,
             params=params,
@@ -70,17 +83,50 @@ class ALFACoins:
         return result
 
     def get_rates(self):
-        return self.request('GET', 'rates')
+        """
+        Get rate for all available pairs
+        """
+        return self._request('GET', 'rates')
 
     def get_rate(self, pair):
-        return self.request('GET', f'rate/{pair}')
+        """
+        Get rate for pair
+
+        :param pair: Cryptocurrency and fiat Pair
+        """
+        return self._request('GET', f'rate/{pair}')
 
     def get_fees(self):
-        return self.request('GET', 'fees')
+        """
+        Get all gate fees for deposit and withdrawal
+        """
+        return self._request('GET', 'fees')
 
     def bitsend(self, type, options, recipient_email=None, amount=None,
             coin_amount=None, recipient_name=None, reference=None):
-
+        """
+        BitSend primary use to payout salaries for staff or making direct
+            deposits to different cryptocurrency addresses
+        :param type: Cryptocurrency to pay with
+        :param options: (array) Client cryptocurrency address for deposit,
+            and additional tags i.e.
+            {"address": "1FE7bSYsXSMrdXTCdRUWUB6jGFFba74fzm"} for Bitcoin,
+            Litecoin, Ethereum, Dash
+            {
+                "address": "qFE7bSYsXSMrdXTCdRUWUB6jGFFba74fzm",
+                "legacy_address": "1FE7bSYsXSMrdXTCdRUWUB6jGFFba74fzm"
+            }
+            for Bitcoin Cash {
+                "address": "rExZpwNwwrmFWbX81AqbKJYkq8W6ZoeWE6",
+                "destination_tag": "1294967290"
+            } for XRP
+        :param amount: Deposit amount in merchant's fiat currency (optional)
+        :param coin_amount: Deposit amount in selected cryptocurrency
+            (optional)
+        :param recipient_name: Client Name (for email notification)
+        :param recipient_email: Client email (for email notification)
+        :param reference: Deposit description (for client notification)
+        """
         if amount is None and coin_amount is None:
             raise TypeError('One of amount or coin_amount must be passed')
 
@@ -93,10 +139,15 @@ class ALFACoins:
             reference=reference
         )
 
-        return int(self.request('POST', 'bitsend', json_data=data)['id'])
+        return int(self._request('POST', 'bitsend', json_data=data)['id'])
 
-    def bitsend_status(self, bitsend_id):
-        return self.request(
+    def bitsend_status(self, bitsend_id: int):
+        """
+        BitSend status primary use to get information of bitsend payout
+
+        :params bitsend_id: (int) Bitsend ID
+        """
+        return self._request(
             'POST',
             'bitsend_status',
             json_data=dict(bitsend_id = bitsend_id),
@@ -104,6 +155,20 @@ class ALFACoins:
 
     def create_order(self, type, amount, order_id, options, currency=None,
             description=None):
+        """
+        Create order for payment
+
+        :param order_id: Merchant's Order ID
+        :param description: (Optional) Description for order
+        :param options: (Optional) Array {
+            "notificationURL": "[custom Merchant's URL for paymentnotification]
+            ",
+            "redirectURL": "[Merchant's page which is shown after
+                 payment is made by a customer]",
+            "payerName": "[Customer's name for notification]",
+            "payerEmail": "[Customer's email for notification]"
+        }
+        """
         data = dict(
             type=type,
             amount=amount,
@@ -112,16 +177,42 @@ class ALFACoins:
             currency=currency,
             description=description
         )
-        return self.request('POST', 'create', json_data=data)
+        return self._request('POST', 'create', json_data=data)
 
     def order_status(self, txn_id):
-        return self.request('POST', 'status', json_data=dict(txn_id=txn_id))
+        """
+        Get status of created Order
+
+        :param txn_id: ALFAcoins TXN ID
+        """
+        return self._request('POST', 'status', json_data=dict(txn_id=txn_id))
 
     def statistics(self):
-        return self.request('POST', 'stats')
+        return self._request('POST', 'stats')
 
     def refund(self, txn_id, options={}, address='', amount=None,
                new_rate=False):
+        """
+        Refund completed order
+
+        :param txn_id: ALFAcoins TXN ID
+        :param options: (array) Client cryptocurrency address for deposit,
+            and additional tags i.e.
+                {"address": "1FE7bSYsXSMrdXTCdRUWUB6jGFFba74fzm"}
+                    for Bitcoin, Litecoin, Ethereum, Dash
+                {
+                    "address": "qFE7bSYsXSMrdXTCdRUWUB6jGFFba74fzm",
+                    "legacy_address": "1FE7bSYsXSMrdXTCdRUWUB6jGFFba74fzm"
+                } for Bitcoin Cash
+                {
+                    "address": "rExZpwNwwrmFWbX81AqbKJYkq8W6ZoeWE6",
+                    "destination_tag": "1294967290"
+                } for XRP
+                :param amount: (Optional) Amount to refund, must be
+                    less Order amount, If omitted full amount will be refunded
+                :param new_rate: (Optional) Use current time rates for fiat to
+                    cryptocurrency conversion or use order's rate
+        """
         if not address and not options:
             raise KeyError('One of options or address is requierd')
 
@@ -131,5 +222,5 @@ class ALFACoins:
             amount=amount,
             new_rate=new_rate
         )
-        return self.request('POST', 'refund', json_data=data)
+        return self._request('POST', 'refund', json_data=data)
 
